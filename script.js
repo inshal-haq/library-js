10529e7dfff2cef75ac3a523accc9174b2baf4c1
Inshal Haq <inshalulhaq11@gmail.com>
2023-10-08 18:20:32 -0500

    Generate html to show books using showBooks()

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const myLibrary = [];
+const myLibrary = [{
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    pages: 295,
+    read: false
+  }, {
+    title: 'Moby Dick',
+    author: 'Herman Melville',
+    pages: 752,
+    read: true
+  }
+];
+
+showBooks();
 
 // constructor
 function Book(title, author, pages, read) {
@@ -13,4 +26,23 @@ Book.prototype.info = function() {
   return (this.read) ? 
     `${this.title} by ${this.author}, ${this.pages} pages, have read` :
     `${this.title} by ${this.author}, ${this.pages} pages, not read yet` ;
+}
+
+
+function showBooks() {
+  let booksHTML = '';
+
+  myLibrary.forEach(book => {
+    booksHTML += `
+      <div class="book-card">
+        <div class="column-item">${book.title}</div>
+        <div class="column-item">${book.author}</div>
+        <div class="column-item">${book.pages}</div>
+        <div class="column-item">${book.read}</div>
+      </div>
+    `;
+  });
+
+  document.querySelector('.books')
+    .innerHTML = booksHTML;
 }
